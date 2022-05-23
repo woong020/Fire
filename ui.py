c1daa6f2b24a6d01ac426ea1126685a3c8bf8913
woong020 <parkhw98@ajou.ac.kr>
2022-05-24 00:07:36 +0900

    220523 UI 구상 창, 버튼 설정 추가

--- ui.py
+++ ui.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QAction, qApp
+from PyQt5.QtWidgets import *
+#from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QAction, qApp
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QCoreApplication
 
@@ -16,6 +17,16 @@ class Main_Window(QMainWindow):
         btn_quit.move(420, 620)
         btn_quit.resize(btn_quit.sizeHint())
         btn_quit.clicked.connect(QCoreApplication.instance().quit)
+
+        btn_loadfile = QPushButton('Load', self)
+        btn_loadfile.move(50, 50)
+        btn_loadfile.clicked.connect(self.initLOAD)
+
+    # FileLoad def
+    def initLOAD(self):
+        fname = QFileDialog.getOpenFileName(self, '', '', 'All File(*);; Exel Data(*.csv)')
+        print(fname[0])
+        print(fname[1])
 
     # StatusBar initial
     def initSTATUS(self):
@@ -36,10 +47,13 @@ class Main_Window(QMainWindow):
     def initUI(self):
         self.setWindowTitle('YANA')
         self.setWindowIcon(QIcon('icon.png'))
-        self.setGeometry(300, 300, 515, 659)
+        self.setGeometry(300, 300, 515, 660)
+        self.setFixedSize(515, 660)
         Main_Window.initBTN(self)
         Main_Window.initSTATUS(self)
         Main_Window.initMENU(self)
+
         #self.move(300, 300)
         #self.resize(515,659)
         self.show()
+
