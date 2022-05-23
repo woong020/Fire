607b1e28910e553076a09bcfed74eaabc9afd6a3
woong020 <parkhw98@ajou.ac.kr>
2022-05-23 23:34:15 +0900

    220523 UI 구상

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-# 샘플 Python 스크립트입니다.
+# Souce import
+import ui
 
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+# Pakage import
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton, QAction, qApp
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
 
-
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
-
-
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
+# Main code
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
-
-
-## main place
-## Test
-## Commit Test
-
-## woong test
+    window = QApplication(sys.argv)
+    ex = ui.Main_Window()
+    sys.exit(window.exec_())
