0dea51501d878e2de0c0bd1fd122164d729b427b
woong020 <parkhw98@ajou.ac.kr>
2022-05-27 01:20:46 +0900

    220526 ui구성추가

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
 # Source import
 import ui
+import FireUI
 
 # Pakage import
 import sys
-from PyQt5.QtWidgets import QApplication #, QMainWindow, QWidget, QPushButton, QAction, qApp
-#from PyQt5.QtGui import QIcon
-#from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import QCoreApplication
+
 
 # Main code
-if __name__ == '__main__':
-    window = QApplication(sys.argv)
-    ex = ui.Main_Window()
-    sys.exit(window.exec_())
+if __name__ == "__main__" :
+    #QApplication : 프로그램을 실행시켜주는 클래스
+    app = QApplication(sys.argv)
+    #WindowClass의 인스턴스 생성
+    myWindow = ui.WindowClass()
+    #프로그램 화면을 보여주는 코드
+    myWindow.show()
+    #프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
+    app.exec_()
+
+# if __name__ == "__main__" :
+#     #QApplication : 프로그램을 실행시켜주는 클래스
+#     app = QApplication(sys.argv)
+#     #WindowClass의 인스턴스 생성
+#     mainWindow = QMainWindow()
+#     #프로그램 화면을 보여주는 코드
+#     mywindow = FireUI.Ui_Dialog()
+#     mywindow.setupUi(mainWindow)
+#     mainWindow.show()
+#     #프로그램을 이벤트루프로 진입시키는(프로그램을 작동시키는) 코드
+#     app.exec_()
