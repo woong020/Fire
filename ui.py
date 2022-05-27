4403ac039b5e953758dab069cf2547cf91eb68ad
woong020 <parkhw98@ajou.ac.kr>
2022-05-27 16:05:42 +0900

    220527 about 추가

--- ui.py
+++ ui.py
@@ -32,9 +32,10 @@ class WindowClass(QMainWindow, form_class) :
 
     # Button initial
     def initBTN(self):
-
         self.btnLOADFILE1.clicked.connect(lambda : self.initBtnFileLoad1())
         self.btnLOADFILE2.clicked.connect(lambda : self.initBtnFileLoad2())
+        self.radiobtn_CURVED.clicked.connect(lambda  : self.initRadBtnCurved())
+        self.radiobtn_DISTRIBUTED.clicked.connect(lambda: self.initRadBtnDistributed())
 
     # StatusBar initial
     def initSTATUS(self):
@@ -42,23 +43,51 @@ class WindowClass(QMainWindow, form_class) :
 
     # MenuBar initial
     def initMENU(self):
+        # Menu Action initial
         exitAction = QAction('Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.triggered.connect(QCoreApplication.instance().quit)
 
+        abouttext = "This program is a hands-on program for data analysis in the first semester of 2022 and was produced by Team Fire.\n" \
+                    "It cannot be copied or used without permission and requires the consent of the manufacturer.\n\n " \
+                    "Powerd by 오픈 소스 소프트웨어 기반\n" \
+                    " Copyright ⓒ 2022 Team Fire"
+        aboutAction = QAction('About', self)
+        aboutAction.setShortcut('Ctrl+H')
+        aboutAction.triggered.connect(lambda : QMessageBox.about(self, 'About', abouttext))
+
+
+        # Menu bar
         menubar = self.menuBar()
         menubar.setNativeMenuBar(False)
+
         filemenu = menubar.addMenu('&File')
         filemenu.addAction(exitAction)
+        helpmenu = menubar.addMenu('&Help')
+        helpmenu.addAction(aboutAction)
 
     # 파일 선택 1
     def initBtnFileLoad1(self):
         global fname1
         fname1 = QFileDialog.getOpenFileName(self, '', '', 'All File(*);; Exel Data(*.csv)')
         self.labelgetFILE1.setText(fname1[0])
+        self.statusBar().showMessage('File1 Ready')
 
     # 파일 선택 2
     def initBtnFileLoad2(self):
         global fname2
         fname2 = QFileDialog.getOpenFileName(self, '', '', 'All File(*);; Exel Data(*.csv)')
         self.labelgetFILE2.setText(fname2[0])
+        self.statusBar().showMessage('File2 Ready')
+
+    # 꺾은선 선택
+    def initRadBtnCurved(self):
+        global lineopt
+        lineopt = 0
+        print(lineopt)
+
+    # 분산형 선택
+    def initRadBtnDistributed(self):
+        global lineopt
+        lineopt = 1
+        print(lineopt)
