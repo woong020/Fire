120ee6aeca9bdf7610776691d2480de3d15eb7dd
woong020 <parkhw98@ajou.ac.kr>
2022-05-27 21:20:10 +0900

    220527 closeevent 추가

--- ui.py
+++ ui.py
@@ -1,28 +1,31 @@
-import matplotlib.pyplot as plt
-#import graph
+# import .py
 
+# import package
 import sys
+import os.path
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import QCoreApplication, QSize, Qt
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import matplotlib.pyplot as plt
 # from mpl_toolkits.mplot3d import Axes3D
 # from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 # from matplotlib.backends.backend_qt5agg import FigureCanvasQT as FigureCanvas
 # from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 # from matplotlib.figure import Figure
 
+
+
+
 # UI파일 연결
-import ui
-
 form_class = uic.loadUiType("FireUI.ui")[0]
-
-
 
 class About(QWidget):
     def initUI(self):
         self.setWindowTitle('Fire')
         self.setWindowIcon(QIcon('.ico\\Fire.png'))
+        self.show()
+
 
 #화면을 띄우는데 사용되는 Class 선언
 class WindowClass(QMainWindow, form_class) :
@@ -38,7 +41,17 @@ class WindowClass(QMainWindow, form_class) :
         self.initSTATUS()
         self.initMENU()
         self.initBTN()
+        self.initCheckTrend()
+        self.initLOGO()
 
+    # Logo initial
+    def initLOGO(self):
+        # logo.png locate find
+        path_dir = os.getcwd()
+        logo_dir = path_dir + "\\.ico\\logo.png"
+        logo = QPixmap(logo_dir)
+        logo_img = logo.scaled(QSize(100, 100), aspectRatioMode=Qt.KeepAspectRatio)
+        self.label_LOGO.setPixmap(logo_img)
 
     # Button initial
     def initBTN(self):
@@ -56,7 +69,8 @@ class WindowClass(QMainWindow, form_class) :
         # Menu Action initial
         exitAction = QAction('Exit', self)
         exitAction.setShortcut('Ctrl+Q')
-        exitAction.triggered.connect(QCoreApplication.instance().quit)
+        exitAction.triggered.connect(lambda : self.close())
+        # exitAction.triggered.connect(QCoreApplication.instance().quit)
 
         abouttext = "This program is a hands-on program for data analysis in the first semester of 2022 and was produced by Team Fire.\n" \
                     "It cannot be copied or used without permission and requires the consent of the manufacturer.\n\n " \
@@ -76,43 +90,42 @@ class WindowClass(QMainWindow, form_class) :
         helpmenu = menubar.addMenu('&Help')
         helpmenu.addAction(aboutAction)
 
-    # 파일 선택 1
+    # Select File 1
     def initBtnFileLoad1(self):
         global fname1
         fname1 = QFileDialog.getOpenFileName(self, '', '', 'All File(*);; Exel Data(*.csv)')
         self.labelgetFILE1.setText(fname1[0])
         self.statusBar().showMessage('File1 Ready')
 
-    # 파일 선택 2
+    # Select File 2
     def initBtnFileLoad2(self):
         global fname2
         fname2 = QFileDialog.getOpenFileName(self, '', '', 'All File(*);; Exel Data(*.csv)')
         self.labelgetFILE2.setText(fname2[0])
         self.statusBar().showMessage('File2 Ready')
 
-    # 꺾은선 선택
+    # Select Radio 1
     def initRadBtnCurved(self):
         lineopt = 0
         WindowClass.initLineOpt(lineopt)
-        print(lineopt)
 
-    # 분산형 선택
+    # Select Radio 2
     def initRadBtnDistributed(self):
         lineopt = 1
         WindowClass.initLineOpt(lineopt)
-        print(lineopt)
 
-    # 라디오 버튼 옵션값, 문제 시 전역변수로 변경할 것
+    # Radio Button Option Value
     def initLineOpt(self):
         opt = self
+        # print(opt)
 
-    # 추세선 체크박스
+    # select CheckBox (default : Check)
     def initCheckTrend(self):
         self.checkBox_TREND.toggle()
 
-    # 종료이벤트
-    def initCloseEvent(self, event):
-        reply = QMessageBox.question(self, 'Message', 'Are you sure to quit?',
+    # Close Event
+    def closeEvent(self, event,):
+        reply = QMessageBox.question(self, 'EXIT', 'Are you sure to quit?',
                                     QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
 
         if reply == QMessageBox.Yes:
