f25ecb538689952889c9595d7deeebaaed7311cc
woong020 <parkhw98@ajou.ac.kr>
2022-06-03 02:47:20 +0900

    220603 산점도그래프 추가중

--- graph.py
+++ graph.py
@@ -5,99 +5,111 @@ import ui
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 ### 시도별 각 자료 데이터 프레임 추가하기
 
-lonely_death = pd.read_csv(r'C:\Users\BJM\Desktop\학교\22-1학기\데이터분석프로그래밍기초\조별과제\0530\시도별 무연고 사망자.csv', encoding='cp949') #lonely death
-lonely_death1 =pd.DataFrame(lonely_death)
+def setData2Frame(self):
+    data = pd.read_csv(self, encoding='cp949')
+    df = pd.DataFrame(data)
+    return df
 
-BLR = pd.read_csv(r'C:\Users\BJM\Desktop\학교\22-1학기\데이터분석프로그래밍기초\조별과제\0530\시도별 기초생활수급자.csv', encoding='cp949') #Basic Livelihood Recipeint
-BLR1 =pd.DataFrame(BLR)
+def setDataCsv(data_path_1, data_path_2, data_path_3, data_path_4):
+    data1 = setData2Frame(data_path_1)
+    data2 = setData2Frame(data_path_2)
+    data3 = setData2Frame(data_path_3)
+    data4 = setData2Frame(data_path_4)
+    return data1, data2, data3, data4
 
-oph = pd.read_csv(r'C:\Users\BJM\Desktop\학교\22-1학기\데이터분석프로그래밍기초\조별과제\0530\시도별 1인 가구 현황.csv', encoding='cp949') #One-person households
-oph1 =pd.DataFrame(oph)
+def setFrame2Inf(data1, data2, data3, data4):
+    opt = [data1, data2, data3, data4]  # 각 옵션별 데이터 프레임
+    opt_corr = [data1, data2, data3, data4]  # 각 옵션별 상관계수 설정을 위한 데이터 프레임
 
-uep = pd.read_csv(r'C:\Users\BJM\Desktop\학교\22-1학기\데이터분석프로그래밍기초\조별과제\0530\시도별 실업자.csv', encoding='cp949') #unemployed person
-uep1 =pd.DataFrame(uep)
+    name = ['무연고', '기초생활수급자', '1인가구', '실업자']  # 특정 지역과 옵션을 묶기 위해 리스트 작성 1
+    local = ['전국', '서울', '강원', '경기', '경남', '경북', '광주', '대구', '대전',
+             '부산', '세종', '울산', '인천', '전북', '전남', '제주', '충북', '충남']  # 특정 지역과 옵션을 묶기 위해 리스트 작성 2
 
-opt = [lonely_death1, BLR1, oph1, uep1] # 각 옵션별 데이터 프레임
-opt_corr = [lonely_death1, BLR1, oph1, uep1] # 각 옵션별 상관계수 설정을 위한 데이터 프레임
-name = ['무연고', '기초생활수급자', '1인가구', '실업자'] # 특정 지역과 옵션을 묶기 위해 리스트 작성 1
-local = ['전국', '서울', '강원', '경기', '경남', '경북', '광주', '대구', '대전',
-         '부산', '세종', '울산', '인천', '전북', '전남', '제주', '충북', '충남'] #특정 지역과 옵션을 묶기 위해 리스트 작성 2
-df_local = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #지역을 필터로 4개 옵션 비교를 위해 리스트 작성
-df_local_corr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] #지역을 필터로 4개 옵션 비교 상관계수를 설정하기 위해 리스트 작성
+    df_local = []
+    df_local_corr = []
+    for i in range(18):
+        df_local.append(0)
+        df_local_corr.append(0)
 
-
-#옵션별 데이터 행열 변환
-for i in range(len(opt)):
-    df = opt[i].set_index('시도별') #인덱스 시도별 열로 변경
-    opt[i] = df.transpose() #행열 전환
-#    print(opt[i])
-#    print(opt[i].isnull().sum()) #결측치 확인
-
-#지역별 데이터 인덱스 설정 후 행열 변환
-for i in range(len(local)):
-    df_local[i] = pd.DataFrame()
-    df_local[i] = uep1.set_index('시도별')
-    df_local[i] = df_local[i].transpose() #행열 전환
-
-
-#지역별 상관관계 생성
-for i in range(len(name)):
-    for j in range(len(local)):
-        df_local[j][name[i]+'_'+local[j]] = opt[i][local[j]]
+    newopt = []
+    # 옵션별 데이터 행열 변환
+    for i in range(len(opt)):
+        df = opt[i].set_index('시도별')  # 인덱스 시도별 열로 변경
+        opt[i] = df.transpose()  # 행열 전환
+        newopt.append(opt[i])
 
 
-#지역별 상관관계를 위해 전국 열 삭제
-for i in range(len(local)):
-    df_local[i].drop(local, axis=1, inplace=True)
+    # 지역별 데이터 인덱스 설정 후 행열 변환
+    for i in range(len(local)):
+        df_local[i] = pd.DataFrame()
+        df_local[i] = data4.set_index('시도별')
+        df_local[i] = df_local[i].transpose()  # 행열 전환
+
+    # 지역별 상관관계 생성
+    for i in range(len(name)):
+        for j in range(len(local)):
+            df_local[j][name[i] + '_' + local[j]] = opt[i][local[j]]
+
+    # 지역별 상관관계를 위해 전국 열 삭제
+    for i in range(len(local)):
+        df_local[i].drop(local, axis=1, inplace=True)
+
+    for i in range(len(local)):
+        df_local_corr[i] = df_local[i].corr(method='pearson')  # 전국 무연고 사망자 기준 상관계수 분석(기초생활수급자/1인가구/실업자)
+
+    opt_corr[0] = opt[0].corr(method='pearson')  # 지역별 무연고 사망자 상관계수 분석(전국~충남)
+
+    opt = [data1, data2, data3, data4]
+
+    return df_local, df_local_corr, newopt
 
 
-#print(opt[0]) #데이터 출력 확인
-#print(df_local[0]) #데이터 출력 확인
+def setInf2Heat(df_local_corr, i, ax):
+    sns.heatmap(df_local_corr[i], annot = True, fmt = '.6f', linewidths = .5, cbar_kws={"shrink": .5},
+                cmap = 'RdYlBu_r', vmin = -1, vmax =1, ax = ax)
+    ax.set_title("<1인가구와 고독사 현황>")
+    # ax.set_xlabel("x")
+    # ax.set_xlabel("y")
 
-#print(df_local[0].isnull().sum()) #데이터 결측치 확인
 
-opt_corr[0] = opt[0].corr(method='pearson') #지역별 무연고 사망자 상관계수 분석(전국~충남)
-df_local_corr[0] = df_local[0].corr(method='pearson') #전국 무연고 사망자 기준 상관계수 분석(기초생활수급자/1인가구/실업자)
+def setInf2Reg(df_local, i, j, opt, ax):
+    sns.regplot(x = df_local[i].iloc[:, [0]], y = df_local[0].iloc[:, [j]], data=opt[0], line_kws={'color': 'red'})
+    ax.set_title("<Scatter of 무연고 사망자와 1인가구>")
 
-#df_corr = opt[0][['무연고 사망자', '1인가구']].corr(method='pearson') #특정 열끼리 상관계수 분석 방법 1
-#df_corr = df_local[0].iloc[:,[0,3]].corr(method='pearson') #test[0]의 0~1 열끼리 상관분석 # 특정 열끼리 상관계수 분석 방법 2
 
-#print(opt_corr[0])  #데이터 출력 확인
-#print(df_local_corr[0])  #데이터 출력 확인
-#opt_corr[0].to_csv('testtesttest_corr.csv', index = False) # 데이터 csv 자료 저장
-#df_local_corr[0].to_csv('testtest_corr.csv', index = False) # 데이터 csv 자료 저장
+
+
 
 
 #상관계수 HeatMap############################################
-plt.figure(figsize = (13,10))
-plt.rc('font', family='Malgun Gothic') #그래프 한국어 설정
-
-# 지역별 무연고 사망자와 그 외 옵션 상관계수 HEATMAP
-# df_local_corr[0번부터 17번]까지
-# ['전국(0)', '서울(1)', '강원(2)', '경기(3)', '경남(4)', '경북(5)', '광주(6)', '대구(7)', '대전(8)',
-#  '부산(9)', '세종(10)', '울산(11)', '인천(12)', '전북(13)', '전남(14)', '제주(15)', '충북(16)', '충남(17)']
-sns.heatmap(df_local_corr[0], annot = True, fmt = '.6f', linewidths = .5, cbar_kws={"shrink": .5}, cmap = 'RdYlBu_r', vmin = -1, vmax =1)
-
-plt.title("<1인가구와 고독사 현황>", fontsize = 23)
-plt.show()
-
-
-#산점도 그래프##############################################
-plt.figure(figsize = (13,10))
-plt.rc('font', family='Malgun Gothic') #그래프 한국어 설정
-
-# df_local[0번부터 17번]까지
-# ['전국(0)', '서울(1)', '강원(2)', '경기(3)', '경남(4)', '경북(5)', '광주(6)', '대구(7)', '대전(8)',
-#  '부산(9)', '세종(10)', '울산(11)', '인천(12)', '전북(13)', '전남(14)', '제주(15)', '충북(16)', '충남(17)']
-# df_local[].iloc[:,[0번부터 3번까지]]
-# ['무연고(0)', '기초생활수급자(1)', '1인가구(2)', '실업자(3)']
-
-# 지역별 무연고 사망자 [0]번의 무연고 사망자와 기초생활수급자 산점도
-a = sns.regplot(x = df_local[0].iloc[:,[0]], y = df_local[0].iloc[:,[1]], data=opt[0], line_kws={'color': 'red'})
-a.set_title('Scatter of 무연고 사망자와 1인가구')
-
-plt.show()
+# plt.figure(figsize = (13,10))
+# plt.rc('font', family='Malgun Gothic') #그래프 한국어 설정
+#
+# # 지역별 무연고 사망자와 그 외 옵션 상관계수 HEATMAP
+# # df_local_corr[0번부터 17번]까지
+# # ['전국(0)', '서울(1)', '강원(2)', '경기(3)', '경남(4)', '경북(5)', '광주(6)', '대구(7)', '대전(8)',
+# #  '부산(9)', '세종(10)', '울산(11)', '인천(12)', '전북(13)', '전남(14)', '제주(15)', '충북(16)', '충남(17)']
+# sns.heatmap(df_local_corr[0], annot = True, fmt = '.6f', linewidths = .5, cbar_kws={"shrink": .5}, cmap = 'RdYlBu_r', vmin = -1, vmax =1)
+#
+# plt.title("<1인가구와 고독사 현황>", fontsize = 23)
+# plt.show()
+#
+#
+# #산점도 그래프##############################################
+# plt.figure(figsize = (13,10))
+# plt.rc('font', family='Malgun Gothic') #그래프 한국어 설정
+#
+# # df_local[0번부터 17번]까지
+# # ['전국(0)', '서울(1)', '강원(2)', '경기(3)', '경남(4)', '경북(5)', '광주(6)', '대구(7)', '대전(8)',
+# #  '부산(9)', '세종(10)', '울산(11)', '인천(12)', '전북(13)', '전남(14)', '제주(15)', '충북(16)', '충남(17)']
+# # df_local[].iloc[:,[0번부터 3번까지]]
+# # ['무연고(0)', '기초생활수급자(1)', '1인가구(2)', '실업자(3)']
+#
+# # 지역별 무연고 사망자 [0]번의 무연고 사망자와 기초생활수급자 산점도
+# a = sns.regplot(x = df_local[0].iloc[:,[0]], y = df_local[0].iloc[:,[1]], data=opt[0], line_kws={'color': 'red'})
+# a.set_title('Scatter of 무연고 사망자와 1인가구')
+#
+# plt.show()
--- ui.py
+++ ui.py
@@ -1,4 +1,5 @@
 # import .py
+import graph
 
 # import package
 import sys
@@ -7,14 +8,15 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import QCoreApplication, QSize, Qt
 from PyQt5 import QtCore, QtGui, QtWidgets, uic
+import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import FigureCanvasQT as FigureCanvas
+# from matplotlib.backends.backend_qt5agg import FigureCanvasQT as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
-
-
 
 
 # UI파일 연결
@@ -46,8 +48,8 @@ class WindowClass(QMainWindow, form_class) :
         self.initSTATUS()
         self.initMENU()
         self.initBTN()
-        # self.initCheckTrend()
         self.initLOGO()
+        self.initPlotGraph()
 
     # Logo initial
     def initLOGO(self):
@@ -60,12 +62,15 @@ class WindowClass(QMainWindow, form_class) :
 
     # Button initial
     def initBTN(self):
-        self.btnLOADFILE.clicked.connect(lambda : self.initBtnFileLoad())
-        self.btnRESET.clicked.connect(lambda : self.initBtnFileReset())
+        self.btn_LOADFILE.clicked.connect(lambda : self.initBtnFileLoad())
+        self.btn_RESET.clicked.connect(lambda : self.initBtnFileReset())
+        self.btn_RUN.clicked.connect(lambda: self.initBtnRun())
         self.radiobtn_HEATMAP.toggle()
         self.checkBox_TREND.setEnabled(False)
+        self.comboBoxCONTENTS.setEnabled(False)
         self.radiobtn_HEATMAP.clicked.connect(lambda  : self.initRadBtnHeatmap())
         self.radiobtn_GRAPH.clicked.connect(lambda: self.initRadBtnGraph())
+
 
     # StatusBar initial
     def initSTATUS(self):
@@ -77,7 +82,6 @@ class WindowClass(QMainWindow, form_class) :
         exitAction = QAction('Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.triggered.connect(lambda : self.close())
-        # exitAction.triggered.connect(QCoreApplication.instance().quit)
 
         abouttext = "This program is a hands-on program for data analysis in the first semester of 2022 and was produced by Team Fire.\n" \
                     "It cannot be copied or used without permission and requires the consent of the manufacturer.\n\n " \
@@ -86,6 +90,7 @@ class WindowClass(QMainWindow, form_class) :
         aboutAction = QAction('About', self)
         aboutAction.setShortcut('Ctrl+H')
         aboutAction.triggered.connect(lambda : QMessageBox.about(self, 'About', abouttext))
+
 
         # Menu bar
         menubar = self.menuBar()
@@ -121,7 +126,7 @@ class WindowClass(QMainWindow, form_class) :
             self.statusBar().showMessage('File4 Ready')
             filecnt += 1
         else:
-            QMessageBox.information(self, "Error", "Already all file")
+            QMessageBox.information(self, "Error", "All files are ready")
 
 
     # Select File Reset
@@ -133,24 +138,130 @@ class WindowClass(QMainWindow, form_class) :
         self.labelgetFILE2.setText(' ')
         self.labelgetFILE3.setText(' ')
         self.labelgetFILE4.setText(' ')
-
+        self.fig.clear()
+        self.canvas.draw()
+        self.radiobtn_HEATMAP.toggle()
+        self.checkBox_TREND.setEnabled(False)
+        self.comboBoxCONTENTS.setEnabled(False)
+        self.statusBar().showMessage('Reset All')
 
     # Select Radio 1
     def initRadBtnHeatmap(self):
-        global lineopt
-        lineopt = 0
         self.checkBox_TREND.setEnabled(False)
-
+        self.comboBoxCONTENTS.setEnabled(False)
+        self.statusBar().showMessage('select Heatmap')
 
     # Select Radio 2
     def initRadBtnGraph(self):
-        global lineopt
-        lineopt = 1
         self.checkBox_TREND.setEnabled(True)
+        self.comboBoxCONTENTS.setEnabled(True)
+        self.statusBar().showMessage('select Graph')
 
+    # initial Graph ToolBar
+    def initToolBar(canvas, self):
+        toolbar = NavigationToolbar(canvas, self.widget_GRAPH)
+        return toolbar
+
+    # initial Graph
     def initPlotGraph(self):
-        fig = plt.Figure()
-        canvas = FigureCanvas(fig)
+        self.fig = plt.Figure()
+        plt.rc("font", family="Malgun Gothic")
+        sns.set(font="Malgun Gothic",
+                rc={"figure.figsize":(5, 5), "axes.unicode_minus": False}, style='darkgrid')
+
+        self.canvas = FigureCanvas(self.fig)
+        toolbar = WindowClass.initToolBar(self.canvas, self)
+        self.canvas.draw()
+
+        self.verticalLayout_plot.addWidget(toolbar)
+        self.verticalLayout_plot.addWidget(self.canvas)
+
+    # Run Button initial
+    def initBtnRun(self):
+        combo_area = self.comboBoxAREA.currentText()
+        if combo_area == 'Select':
+            QMessageBox.information(self, "Error", "Please Select Area")
+
+        else:
+            areacode = WindowClass.initComboArea(combo_area)
+            data1, data2, data3, data4 = graph.setDataCsv(self.labelgetFILE1.text(), self.labelgetFILE2.text(),
+                                                          self.labelgetFILE3.text(), self.labelgetFILE4.text())
+            df_local, df_local_corr, opt = graph.setFrame2Inf(data1, data2, data3, data4)
+            self.fig.clear()
+            if self.radiobtn_HEATMAP.isChecked():
+                ax = self.fig.add_subplot(111)
+                graph.setInf2Heat(df_local_corr, areacode, ax)
+                self.fig.tight_layout()
+                self.canvas.draw()
+                self.statusBar().showMessage('Executed Heatmap')
+
+            else:
+                contentsname = self.comboBoxCONTENTS.currentText()
+                if contentsname == 'Select':
+                    QMessageBox.information(self, "Error", "Please Select Contents")
+                else:
+                    ax = self.fig.add_subplot(111)
+                    contentscode = WindowClass.initComboContents(contentsname)
+                    graph.setInf2Reg(df_local, areacode, contentscode, opt, ax)
+                    # self.fig.tight_layout()
+                    self.canvas.draw()
+
+
+
+
+    # Combobox Area to AreaCode
+    def initComboArea(self):
+        areaname = self
+        if areaname == '전국':
+            areacode = 0
+        elif areaname == '서울':
+            areacode = 1
+        elif areaname == '강원':
+            areacode = 2
+        elif areaname == '경기':
+            areacode = 3
+        elif areaname == '경남':
+            areacode = 4
+        elif areaname == '경북':
+            areacode = 5
+        elif areaname == '광주':
+            areacode = 6
+        elif areaname == '대구':
+            areacode = 7
+        elif areaname == '대전':
+            areacode = 8
+        elif areaname == '부산':
+            areacode = 9
+        elif areaname == '세종':
+            areacode = 10
+        elif areaname == '울산':
+            areacode = 11
+        elif areaname == '인천':
+            areacode = 12
+        elif areaname == '전북':
+            areacode = 13
+        elif areaname == '전남':
+            areacode = 14
+        elif areaname == '제주':
+            areacode = 15
+        elif areaname == '충북':
+            areacode = 16
+        elif areaname == '충남':
+            areacode = 17
+        else:
+            QMessageBox.information(self, "Error", "Please Select Area")
+        return areacode
+
+    def initComboContents(self):
+        contentsname = self
+        if contentsname == '기초생활수급자':
+            contentscode = 1
+        elif contentsname == '1인가구':
+            contentscode = 2
+        elif contentsname == '실업자':
+            contentscode = 3
+
+        return contentscode
 
 
 
